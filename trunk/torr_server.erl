67a5c140c2f3177f594c0e0ce5d59e7d113a4ef5
tobsan <tobsan@9100c256-f436-11dd-aa46-671127399dce>
2009-02-20 10:46:00 +0000

    initial client and server

---
+++ trunk/torr_server.erl
@@ -0,0 +1,25 @@
+-module(torr_server).
+-author("Tobias Olausson & David Hadzic").
+
+-import(gen_tcp,[accept/1,listen/2,controlling_process/2]).
+-import(inet,[peername/1]).
+-import(torr_client,[handle/1]).
+-export([init/1]).
+
+init(Port) ->
+   case listen(Port,[list,{active,true}]) of
+      {ok,Listen} -> loop(Listen);
+      {error,_} -> failed
+   end.
+
+
+loop(Listen) ->
+   case accept(Listen) of
+      {ok,Socket} ->
+         {ok,{IP,_}} = peername(Socket),
+         io:format("IP:~w connected \n",[IP]),
+         Pid = spawn(fun() -> handle(Socket) end),
+         controlling_process(Socket,Pid),
+         loop(Listen);
+      {error,Reason} -> {fail,Reason}
+   end.
