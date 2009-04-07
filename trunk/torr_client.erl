a84442fd6e98672d9322cd2a60a1e5e70ad0b88a
tobsan <tobsan@9100c256-f436-11dd-aa46-671127399dce>
2009-04-07 14:33:12 +0000

    new parser

--- trunk/torr_client.erl
+++ trunk/torr_client.erl
@@ -5,76 +5,30 @@
 -import(string,[str/2,substr/2,substr/3,tokens/2,concat/2]).
 -import(dict,[is_key/2,find/2,store/3,from_list/1]).
 -import(lists,[map/2,any/2,takewhile/2,dropwhile/2,reverse/1,foreach/2]).
+-import(torr_parser,[parse_req/2]).
+-import(bval,[bencode/1]).
+
 -export([handle/1]).
 
 handle(Socket) ->
     receive
         {tcp,Socket,Data} -> 
-            spawn(fun() -> parse(Data,self()) end),
+            spawn(fun() -> parse_req(Data,self()) end),
             handle(Socket);
-        {parsed,Dict} ->
+        {parse_ok,Dict} ->
             case is_valid(Dict) of
                 true    -> ok;
                 false   -> ok
             end,
             handle(Socket);
-        {response,Resp} ->
-            % BEncode
+        {peers,PeerDict} ->
+            Resp = bencode(PeerDict),
             send(Socket,Resp),
             close(Socket);
-        fail -> 
+        parse_fail -> 
             send(Socket,"d7:failure11:Bad requeste"),
             close(Socket)
     end.
 
 % Does it contain info_hash, peer_id and port?
-is_valid(Dict) -> 
-    case is_key(info_hash,Dict) and is_key(peer_id,Dict) of
-        true    -> ok;
-        false   -> ok
-    end.
-
-% Parse a GET request
-parse(GetReq,Pid) ->
-    case str(GetReq,"GET /?") of
-        1 ->
-            Stripped = takewhile(fun(A) -> A /= $ end,substr(GetReq,7)),
-            Parts = tokens(Stripped,"&"),
-            SFun  = fun(Elem) -> tokens(Elem,"=") end,
-            Pairs  = map(SFun,Parts),
-            case any(fun(Elem) -> length(Elem) /= 2 end,Pairs) of
-                true  -> Pid ! fail;
-                false -> 
-                    TupleList = map(fun(E) -> list_to_tuple(E) end, Pairs),
-                    Result = map(fun({Key,Val}) -> 
-                        {list_to_atom(Key),urldecode(Val)} end,TupleList),
-                    Pid ! {parsed, from_list(Result)}
-            end;
-        _ -> Pid ! fail
-    end.
-
-% öh...
-from_hex(Str) -> erlang:list_to_integer(Str,16).
-from_hexStr(Str) ->
-    case length(Str) of
-        0 -> error;
-        1 -> Str;
-        2 -> from_hex(Str);
-        _ -> 
-            Dec = from_hex(substr(Str,1,2)),
-            [ Dec | substr(Str,3) ]
-    end.
-
-% Hehe
-urldecode([]) -> [];
-urldecode(Str) ->
-    case hd(Str) of
-        $% ->
-            Parts = tokens(Str,"%"),
-            F = fun(Part) -> from_hexStr(Part) end,
-            Encode = map(F,Parts),
-            lists:flatten(Encode);
-        _ ->
-            Parts = tokens(Str,"%"),
-            lists:flatten([ hd(Parts) | urldecode(tl(Parts))])
-    end.
+is_valid(Dict) -> true.
---
+++ trunk/torr_parser.erl
@@ -0,0 +1,29 @@
+-module(torr_parser).
+-author("Tobias & David").
+
+-import(string,[tokens/2]).
+-import(lists,[map/2,all/2]).
+-import(dict,[from_list/1,find/2]).
+-import(erlang,[tuple_size/1,list_to_integer/2]).
+
+-export(parseReq/2).
+
+parse_req(String,Pid) ->
+   RawPairs = tokens(String,"&"),
+   KeyValues = map(fun(E) -> list_to_tuple(tokens(E,"=")) end, RawPairs),
+   case all(fun(Tup) -> tuple_size(Tup) =:= 2 end, KeyValues) of
+      false -> Pid ! parse_fail;
+      true ->
+         UrlFun = fun({Key,Val}) -> {Key,url_decode(Val)} end,
+         Dict = from_list(map(UrlFun,KeyValues)),
+         Pid ! {parse_ok,Dict}
+      end.
+
+%% Take care of this later.
+%% Should fail in case of malformed encoded string
+url_decode([]) -> [];
+url_decode([$%,H,L|Tail]) ->
+   Hex = list_to_integer([H,L],16),
+   [Hex | url_decode(Tail)];
+url_decode([H|Tail]) -> [H | url_decode(Tail)].
+   
