117c78bcb2589ca00f6018f94ff48cde6354ae02
tobsan <tobsan@9100c256-f436-11dd-aa46-671127399dce>
2009-04-09 14:27:56 +0000

    some updates

--- torr_client.erl
+++ torr_client.erl
@@ -2,6 +2,7 @@
 -author("Tobias Olausson").
 
 -import(gen_tcp,[send/2,close/1]).
+-import(inet,[peername/1]).
 -import(string,[str/2,substr/2,substr/3,tokens/2,concat/2]).
 -import(dict,[is_key/2,find/2,store/3,from_list/1]).
 -import(lists,[map/2,any/2,takewhile/2,dropwhile/2,reverse/1,foreach/2]).
@@ -12,14 +13,25 @@
 
 handle(Socket) ->
     receive
-        {tcp,Socket,Data} -> 
-            spawn(fun() -> parse_req(Data,self()) end),
+        {tcp,Socket,Data} ->
+            CPid = self(),
+            spawn(fun() -> parse_req(Data,CPid) end),
             handle(Socket);
         {parse_ok,Dict} ->
-            send(Socket,"parse ok"),
-            handle(Socket);
-        fail -> send(Socket,"fail");
+            case is_valid(Dict) of
+                true ->
+                    send(Socket,"parse_ok"),
+                    torr_tracker ! {request,{self(),Dict}},
+                    handle(Socket);
+                false ->
+                    send(Socket,"parse_fail"),
+                    handle(Socket)
+            end;
+        torrent_fail -> 
+            send(Socket,"torrent fail"),
+            close(Socket);
         {peers,PeerDict} ->
+            send(Socket,"peers"),
             Resp = bencode(PeerDict),
             send(Socket,Resp),
             close(Socket);
@@ -29,4 +41,7 @@ handle(Socket) ->
     end.
 
 % Does it contain info_hash, peer_id and port?
-is_valid(Dict) -> true.
+is_valid(Dict) -> is_key(<<"info_hash">>,Dict) and 
+         is_key(<<"peer_id">>,Dict) and
+         is_key(<<"port">>,Dict).
+
--- torr_parser.erl
+++ torr_parser.erl
@@ -11,13 +11,24 @@
 parse_req(String,Pid) ->
    RawPairs = tokens(String,"&"),
    KeyValues = map(fun(E) -> list_to_tuple(tokens(E,"=")) end, RawPairs),
-   case all(fun(Tup) -> tuple_size(Tup) =:= 2 end, KeyValues) of
+   case all(fun(Tup) -> tuple_size(Tup) == 2 end, KeyValues) of
       false -> Pid ! parse_fail;
       true ->
-         UrlFun = fun({Key,Val}) -> {Key,url_decode(Val)} end,
-         Dict = from_list(map(UrlFun,KeyValues)),
-         Pid ! {parse_ok,Dict}
+        Dict = from_list(map(fun(A) -> convert(A) end,KeyValues)),
+        Pid ! {parse_ok,Dict}
       end.
+
+convert({Key,Val}) -> 
+    KBin = list_to_binary(Key),
+    case KBin of
+        <<"port">> -> {KBin,list_to_integer(Val)};
+        <<"uploaded">> -> {KBin,list_to_integer(Val)};
+        <<"downloaded">> -> {KBin,list_to_integer(Val)};
+        <<"left">> -> {KBin,list_to_integer(Val)};
+        _   -> {KBin,list_to_binary(url_decode(Val))}
+    end.
+
+
 
 %% Take care of this later.
 %% Should fail in case of malformed encoded string
@@ -26,4 +37,3 @@ url_decode([$%,H,L|Tail]) ->
    Hex = list_to_integer([H,L],16),
    [Hex | url_decode(Tail)];
 url_decode([H|Tail]) -> [H | url_decode(Tail)].
-   
