ce6d629dc65f1a71d3a64ba6b716409dd15589cc
tobsan <tobsan@9100c256-f436-11dd-aa46-671127399dce>
2009-04-13 10:09:01 +0000

    annandag posk coding

--- trunk/bval.erl
+++ trunk/bval.erl
@@ -1,6 +1,6 @@
 -module(bval). 
--export([bencode/1,bdecode/1]).
-
+-export([bencode/1,bencode_bin/1]).
+-import(dict,[fold/3]).
 
 bencode(List) when is_list(List) ->
 	[$l, [bencode(X) || X <- List], $e];
@@ -13,36 +13,15 @@ bencode(Str) when is_binary(Str) ->
 
 bencode({Key,Val}) -> [bencode(Key),bencode(Val)];
 
+% Somewhat inefficient
 bencode(Dict) when element(1,Dict) == dict ->
 	[$d,[[bencode(Key), bencode(Val)] || {Key, Val} <- dict:to_list(Dict)], $e].
 
-bdecode([$i | Rest]) ->
- 	End = string:chr(Rest, $e),
-	(lists:sublist(Rest,1,End-1));
+% This bencodes peers using the binary model representation
+bencode_bin(Dict) when element(1,Dict) == dict -> 
+   Bin = fold(fun(_Key,Val,Acc) -> bencode_bin_elem(Val,Acc) end, <<>>, Dict),
+   Size = dict:size(Dict) * 6,   
+   {integer_to_list(Size),Bin}.
+%bencode_bin_elem(Tup,Acc) -> io:format("~w",[Tup]), Acc.
+bencode_bin_elem({{_,IP},{_,Port}},Acc) -> <<Acc/binary,IP/binary,Port:16>>.
 
-bdecode([Int | Rest]) when Int >= $0, Int =< $9 ->
-	lists:sublist(Rest,2,Int).
-
-%% bdecode([$l | Rest]) ->
-%% 	decodeList(Rest);
-
-%% bdecode([$d | Rest]) ->
-%% 	decodeDict(Rest).
-
-%% decodeList(List) ->
-%% 	List;
-
-%% decodeDict(Dict) ->
-%% 	Dict.
-
-%% bdecode([Char | Rest]) -> 
-%% 	case Char of
-%% 		{$l} ->  
-
-%%%is_string(Str) -> [and is_char(Char) || Char <- Str].
-
-%% is_string([_Char | _Rest] = Str) when is_list(Str) ->
-%% 	lists:all(fun(C) -> is_char(C) end, Str);
-%% is_string(_) -> false.
-
-%% is_char(Char) -> Char >= 32 andalso Char =< 255.
--- trunk/torr_client.erl
+++ trunk/torr_client.erl
@@ -7,7 +7,7 @@
 -import(dict,[is_key/2,find/2,store/3,from_list/1,to_list/1]).
 -import(lists,[map/2,any/2,takewhile/2,dropwhile/2,reverse/1,foreach/2]).
 -import(torr_parser,[parse_req/2]).
--import(bval,[bencode/1]).
+-import(bval,[bencode/1,bencode_bin/1]).
 
 -export([handle/1]).
 -compile(export_all).
@@ -16,32 +16,34 @@ handle(Socket) ->
     receive
         {tcp,Socket,Data} ->
             CPid = self(),
+            io:format("Received data: ~w\n",[Data]),
             spawn(fun() -> parse_req(Data,CPid) end),
             handle(Socket);
         {parse_ok,Dict} ->
             case is_valid(Dict) of
                 true ->
-                    send(Socket,"parse_ok\n"),
-		    case is_key(<<"ip">>,Dict) of
-			true -> torr_tracker ! {request,{self(),Dict}};
-                    	false ->
-			   {ok,{IP,_}} = peername(Socket),
-			   IPDict = store(<<"ip">>,ip_to_binary(IP),Dict),
-			   torr_tracker ! {request,{self(),IPDict}}
-			end,
-			handle(Socket);
+                  send(Socket,"parse_ok\n"),
+                  case is_key(<<"ip">>,Dict) of
+                     true -> torr_tracker ! {request,{self(),Dict}};
+                     false ->
+                        {ok,{IP,_}} = peername(Socket),
+                        IPDict = store(<<"ip">>,ip2bin(IP),Dict),
+                        torr_tracker ! {request,{self(),IPDict}}
+                  end;
                 false ->
-                    send(Socket,"parse_fail\n"),
-                    handle(Socket)
-            end;
-        torrent_fail -> 
-            send(Socket,"torrent fail\n"),
-            close(Socket);
+                    send(Socket,"parse_fail\n")
+            end,
+            handle(Socket);
+        torrent_fail ->
+            self() ! parse_fail,
+            handle(Socket);
         {peers,PeerDict} ->
-	    send(Socket,"d8:intervali900e5:peers"),
-            send(Socket,bencode(PeerDict)),
-	    send(Socket,"e"),
-	    close(Socket);
+            Pre = "d8:intervali900e5:peers",
+            {Size,Bval} = bencode_bin(PeerDict),
+            Response = Pre ++ Size ++ ":" ++ binary_to_list(Bval) ++ "e",
+            io:format("Sending response: ~w\n",[Response]),
+            send(Socket,Response),
+            close(Socket);
         parse_fail -> 
             send(Socket,"d7:failure11:Bad requeste"),
             close(Socket)
@@ -52,9 +54,9 @@ is_valid(Dict) -> is_key(<<"info_hash">>,Dict) and
          is_key(<<"peer_id">>,Dict) and
          is_key(<<"port">>,Dict).
 
-ip_to_binary({A,B,C,D}) -> list_to_binary(
-	[integer_to_list(A),$.,integer_to_list(B),$.,
-	integer_to_list(C),$.,integer_to_list(D)]).
+ip2bin({A,B,C,D}) -> <<A,B,C,D>>.
+ip_to_binary({A,B,C,D}) -> [integer_to_list(A),$.,integer_to_list(B),$.,
+   integer_to_list(C),$.,integer_to_list(D)].
 
 
 
