3f9b851a19ae58356a8f7139f8895ef7d48b9af0
tobsan <tobsan@9100c256-f436-11dd-aa46-671127399dce>
2009-04-17 12:28:34 +0000

    udp tracker half-done

--- branches/tobsan/torr_client.erl
+++ branches/tobsan/torr_client.erl
@@ -7,7 +7,7 @@
 -import(torr_parser,[parse/2]).
 -import(bval,[bencode/1,bencode_scrape/1]).
 
--export([handle/1]).
+-export([handle_tcp/1,handle_udp/3]).
 
 httpHeader(Type) -> 
    case Type of
@@ -15,20 +15,20 @@ httpHeader(Type) ->
       nf -> "HTTP/1.0 404 Not Found\n\rContent-Type: text/plain\r\n\r\n"
    end.
 
-handle(Socket) ->
+handle_tcp(Socket) ->
    receive
       {tcp,Socket,Data} ->
          CPid = self(),
          spawn_link(fun() -> parse(Data,CPid) end),
-         handle(Socket);
+         handle_tcp(Socket);
       {tcp_closed,Socket} ->
          exit("socket closed");
-      {tcp_error,Socket,Reason} ->
+      {tcp_error,Socket,_Reason} ->
          exit("tcp error");
       % Recieved from the parser
       {{parse_ok,Type},Data} ->
          case Type of
-            scrape -> torr_tracker ! {{request,Type},Data,self()},
+            scrape -> torr_tracker ! {{request,Type},Data,self()};
             announce ->
                case is_key(<<"ip">>,Data) of
                   true -> torr_tracker ! {{request,Type},Data,self()};
@@ -38,7 +38,7 @@ handle(Socket) ->
                      torr_tracker ! {{request,Type},IPData,self()}
                end
          end,
-         handle(Socket);
+         handle_tcp(Socket);
       % All responses comes from the tracker
       {{response,Type},Data} ->
          case Type of
@@ -66,12 +66,36 @@ handle(Socket) ->
          close(Socket);
       % The parser is the only process created by the
       % client. Thus, if something fails, it is the parser
-      {'EXIT',_Parser,normal} -> handle(Socket);
+      {'EXIT',_Parser,normal} -> handle_tcp(Socket);
       {'EXIT',_Parser,Reason} ->
          io:format("Parser crashed: ~w\n",[Reason]),
          send(Socket,httpHeader(ok)),
          send(Socket,<<"d7:failure11:bad requeste">>),
          close(Socket)
    end.
+
+handle_udp(Host,Port,Data) ->
+   Size = bit_size(Data) div 8,
+   case Size >= 16 of
+      false -> exit("bad_packet_size");
+      true -> 
+         <<ConnID:64,Action:32,TransID:32,Rest/binary>> = Data,
+         case ConnID == 41727101980 of
+            false -> exit("not_torrent_packet");
+            true -> 
+               case Action of
+                  % Connection
+                  0 ->
+                     io:format("Sending response\n"),
+                     Response = <<0:32,TransID:32,ConnID:64>>,
+                     udp_server ! {reply,Host,Port,Response};
+                  1 -> ok; %announce
+                  2 -> ok %scrape
+               end
+         end
+   end.
+      
+
+
 
 ip2bin({A,B,C,D}) -> <<A,B,C,D>>.
