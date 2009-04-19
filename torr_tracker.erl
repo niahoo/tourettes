dde6b931a4b4619e32f62ced9a2861172cdf1348
tobsan <tobsan@9100c256-f436-11dd-aa46-671127399dce>
2009-04-19 16:23:55 +0000

    lol dbase

--- torr_tracker.erl
+++ torr_tracker.erl
@@ -1,37 +1,48 @@
 -module(torr_tracker).
--import(dict,[find/2, new/0, store/3, append/3, fetch/2]).
+-author("Tobias Olausson").
+
+-import(dict,[new/0,is_key/2,store/3,erase/2,fetch/2,find/2]).
+-import(lists,[keysearch/3]).
+
 -export([init/0]).
 
 init() ->
-	Pid = spawn(fun() -> tracker(new()) end),
-	register(torr_tracker,Pid),
-	Pid.
+   Pid = spawn_link(fun() -> 
+            process_flag(trap_exit,true),
+            TableID = ets:new(torr_db,[bag,private]),
+            tracker(TableID) 
+         end),
+   register(torr_tracker,Pid),
+   Pid.
 
-tracker(Torrents) ->
-	receive
-		{request,{Cid,ReqDict}} ->
-			Hash = fetch(<<"info_hash">>, ReqDict),
-			case find(Hash,Torrents) of
-				{ok,Peers} -> 
-					Cid ! {peers,Peers},
-					ID = fetch(<<"peer_id">>,ReqDict),
-					case find(ID,Peers) of
-						{ok,_} -> tracker(Torrents);
-						error ->
-                     IP = {<<"ip">>,fetch(<<"ip">>,ReqDict)}, 
-                     Port = {<<"port">>,fetch(<<"port">>,ReqDict)},
-							NewPeers = store(ID,{IP,Port},Peers),
-							tracker(store(Hash,NewPeers,Torrents))
-					end;
-				error ->
-               self() ! {add,Hash}, % Just for test, very open
-            	Cid ! torrent_fail,
-               tracker(Torrents)
-			end;
+tracker(TableID) -> 
+   receive 
+      {{request,Action},Data,Pid} ->
+         case Action of
+            announce -> 
+               Hash  = fetch(<<"info_hash">>,Data), 
+               IP    = fetch(<<"ip">>,Data),
+               Port  = fetch(<<"port">>,Data), 
+               Elem  = <<IP/binary,Port:16>>,
+               Up    = fetch(<<"uploaded">>,Data),
+               Down  = fetch(<<"downloaded">>,Data),
+               Left  = fetch(<<"left">>,Data),
+               
+               case ets:lookup(TableID,Hash) of
+                  []       -> Pid ! {{response,error},<<"not found">>};
+                  TorrData ->
+                     Peers = [ Peer || {_Hash,Peer} <- TorrData],
+                     io:format("Stripped TorrData: ~w\n",[Peers]),
+                     Pid ! {{response,peers},Peers}
+               end,
 
-		{add,Hash} -> 
-		    case find(Hash,Torrents) of
-				{ok,_} -> tracker(Torrents);
-				error ->  tracker(store(Hash,new(),Torrents))
-		    end
-	end.
+               Value = {Elem,Up,Down,Left},
+               ets:insert(TableID,{Hash,Value}),
+               tracker(TableID);
+            
+            scrape -> 
+               Pid ! {{response,scrape_error},<<"Not supported">>},
+               tracker(TableID)
+         end
+   end.
+
