3257b1a7e0f4bb52a876bebfa855c6f0973ca531
tobsan <tobsan@9100c256-f436-11dd-aa46-671127399dce>
2009-04-23 12:59:54 +0000

    ets sort of working

--- torr_tracker.erl
+++ torr_tracker.erl
@@ -23,24 +23,40 @@ tracker(TableID) ->
          case Action of
             announce -> 
                Hash  = fetch(<<"info_hash">>,Data), 
+               io:format("Announcing for hash: ~w\n",[Hash]),
                IP    = fetch(<<"ip">>,Data),
                Port  = fetch(<<"port">>,Data), 
                Elem  = <<IP/binary,Port:16>>,
                Up    = fetch(<<"uploaded">>,Data),
                Down  = fetch(<<"downloaded">>,Data),
                Left  = fetch(<<"left">>,Data),
-               
-               case ets:lookup(TableID,Hash) of
-                  []       -> Pid ! {{response,error},<<"not found">>};
-                  TorrData ->
-                     Peers = [ Peer || {_Hash,Peer} <- TorrData],
-                     Pid ! {{response,peers},Peers}
-               end,
 
-               Value = {Elem,Up,Down,Left},
-               ets:insert(TableID,{Hash,Value}),
+               case ets:lookup(TableID,Hash) of
+                  []       -> 
+                     Pid ! {{response,error},<<"not found">>},
+                     Value = {Elem,Up,Down,Left},
+                     ets:insert(TableID,{Hash,Value});
+                  TorrData ->
+                     io:format("Found Hash\n"),
+                     Peers = [ Peer || {_Hash,Peer} <- TorrData],
+                     Pid ! {{response,peers},Peers},
+                     case find(<<"event">>,Data) of
+                        {ok,3} ->
+                           io:format("Deleting client\n"),
+                           Deleted = lists:keydelete(Elem,1,Peers),
+                           case Deleted of
+                              [] -> ets:delete(TableID,Hash);
+                              _  -> lists:foreach(fun(E) -> ets:insert(TableID,{Hash,E}) end,Deleted)
+                           end;
+                        Other ->
+                           io:format("Action: ~w\n",[Other]),
+                           io:format("Updating client\n"),
+                           Value = {Elem,Up,Down,Left},
+                           ets:insert(TableID,{Hash,Value})
+                     end
+               end,
                tracker(TableID);
-            
+
             scrape -> 
                Pid ! {{response,scrape_error},<<"Not supported">>},
                tracker(TableID)
