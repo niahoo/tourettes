e31903afc19267e4cac9e37a9fb69e8c2aa4cb5c
tobsan <tobsan@gmail.com>
2013-05-01 21:56:53 +0000

    added mnesia module just for fun

--- torr_db_redis.erl
+++ torr_db_redis.erl
@@ -1,4 +1,19 @@
 -module(torr_db_redis).
 -author("Tobias O, David H").
 
+% Startup stuff
+init() -> ok.
 
+% Are we currently tracking this hash?
+hash_exists(Hash) -> ok.
+
+% Add a client to the tracking of a hash. If the hash is not yet tracked, start
+% tracking it, with the one client.
+add_client(Hash,Client) -> ok.
+
+% Gets a list of some number of clients that all are connected to the torrent
+% tracked by Hash.
+get_clients(Hash) -> ok.
+
+% Removes a client from the tracking list for Hash
+remove_client(Hash,Client) -> ok.
--- torr_tracker.erl
+++ torr_tracker.erl
@@ -1,12 +1,20 @@
 -module(torr_tracker).
--author("Tobias Olausson").
+-author("Tobias O, David H").
+
+% TODO: Convert the tracker code to use a more general approach for distributed
+% database system, and let the underlying DBMS take care of the data handling.
+% TODO: OTP-ize it.
 
 -import(dict,[fetch/2,find/2]).
 -export([init/0]).
 
--record(torrent,{
-      info_hash, ip, port,
-      down, up, left, seeding}).
+% Clients are represented with ip, port and some torrent statistics (for a
+% specific torrent, that is! Clients should only be found inside the torrent
+% record. In each torrent, the client field should be a hash table where ip is
+% the key. For a stripped down version, one could remove all data except ip and
+% port from the client record
+-record(client,{ip, port, down, up, left, seeding}).
+-record(torrent,{info_hash,clients = dict:new() }).
 
 init() ->
    Pid = spawn_link(fun() -> 
@@ -18,48 +26,44 @@ init() ->
 
 tracker(TableID) -> 
    receive 
-      {{request,Action},Data,Pid} ->
-         case Action of
-            announce -> 
-               Hash  = fetch(<<"info_hash">>,Data), 
+      {{request,announce},Data,Pid} ->
+           Hash  = fetch(<<"info_hash">>,Data), 
 %               io:format("Announcing for hash: ~w\n",[Hash]),
-               IP    = fetch(<<"ip">>,Data),
-               Port  = fetch(<<"port">>,Data), 
-               Elem  = <<IP/binary,Port:16>>,
-               Up    = fetch(<<"uploaded">>,Data),
-               Down  = fetch(<<"downloaded">>,Data),
-               Left  = fetch(<<"left">>,Data),
+           IP    = fetch(<<"ip">>,Data),
+           Port  = fetch(<<"port">>,Data), 
+           Elem  = <<IP/binary,Port:16>>,
+           Up    = fetch(<<"uploaded">>,Data),
+           Down  = fetch(<<"downloaded">>,Data),
+           Left  = fetch(<<"left">>,Data),
 
-               case ets:lookup(TableID,Hash) of
-                  []       -> 
-                     Pid ! {{response,error},<<"not found">>},
-                     Value = {Elem,Up,Down,Left},
-                     ets:insert(TableID,{Hash,Value});
-                  TorrData ->
+           case ets:lookup(TableID,Hash) of
+              []       -> 
+                 Pid ! {{response,error},<<"not found">>},
+                 Value = {Elem,Up,Down,Left},
+                 ets:insert(TableID,{Hash,Value});
+              TorrData ->
 %                     io:format("Found Hash\n"),
-                     Peers = [ Peer || {_Hash,Peer} <- TorrData],
-                     Pid ! {{response,peers},Peers},
-                     case find(<<"event">>,Data) of
-                        {ok,3} ->
-                           % This is such an ugly hack
+                 Peers = [ Peer || {_Hash,Peer} <- TorrData],
+                 Pid ! {{response,peers},Peers},
+                 case find(<<"event">>,Data) of
+                    {ok,3} ->
+                       % This is such an ugly hack
 %                           io:format("Deleting client\n"),
-                           Deleted = lists:keydelete(Elem,1,Peers),
-                           case Deleted of
-                              [] -> ets:delete(TableID,Hash);
-                              _  -> lists:foreach(fun(E) -> ets:insert(TableID,{Hash,E}) end,Deleted)
-                           end;
-                        Other ->
+                       Deleted = lists:keydelete(Elem,1,Peers),
+                       case Deleted of
+                          [] -> ets:delete(TableID,Hash);
+                          _  -> lists:foreach(fun(E) -> ets:insert(TableID,{Hash,E}) end,Deleted)
+                       end;
+                    _Other ->
 %                           io:format("Action: ~w\n",[Other]),
 %                           io:format("Updating client\n"),
-                           Value = {Elem,Up,Down,Left},
-                           ets:insert(TableID,{Hash,Value})
-                     end
-               end,
-               tracker(TableID);
-
-            scrape -> 
-               Pid ! {{response,scrape_error},<<"Not supported">>},
-               tracker(TableID)
-         end
+                       Value = {Elem,Up,Down,Left},
+                       ets:insert(TableID,{Hash,Value})
+                 end
+           end,
+           tracker(TableID);
+        {{request,scrape},_,Pid} ->
+           Pid ! {{response,scrape_error},<<"Not supported">>},
+           tracker(TableID)
    end.
 
